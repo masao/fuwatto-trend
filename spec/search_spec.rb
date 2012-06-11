4817bd53a6b22df5eb34849d0c7939365a4351cf
Masao Takaku <tmasao@acm.org>
2012-06-12 00:59:06 +0900

    add a test for a normal query.

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -12,4 +12,13 @@ describe "search.rb" do
       result = db.search( "", { :config => config } )
       result[ :pubyear ].should be_empty
    end
+
+   it "should return some results for a normal query." do
+      config_fname = File.join( File.dirname( __FILE__ ), "..", "config.yml" )
+      config = YAML.load( open config_fname )
+      db = Trend::CiNiiArticles.new
+      result = db.search( "portal", { :config => config } )
+      result[ :pubyear ].should_not be_empty
+      result[ :pubyear ].should have_key( 2012 )
+   end
 end
